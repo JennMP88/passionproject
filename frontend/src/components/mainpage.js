4a8e8c699391e75fbe6eff0483afb8b4cdcb2d6b
Jennifer Perez <jenniferperez@pursuit.org>
2019-07-21 14:41:09 -0400

    installed axios and started opening page

--- frontend/src/components/mainpage.js
+++ frontend/src/components/mainpage.js
@@ -40,7 +40,7 @@ class Mainpage extends React.Component {
           <div className='commentaries col col-8'>
          
         
-        <input type="text" onChange={this. handleTextInput} placeholder={"story..."} />
+        <textarea type="text" onChange={this. handleTextInput} placeholder={"story..."} />
         <button type="button"  onClick={this.onSubmitClicker}>Submit</button>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             
--- frontend/src/components/opening.js
+++ frontend/src/components/opening.js
@@ -1,22 +1,85 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/opening.css';
+const openingPic = require('../assets/tv.jpg')
 
-class Opening extends React.Component{
-  constructor (props){
+class Opening extends React.Component {
+  constructor(props) {
     super(props)
-    this.state={
-      
+    this.state = {
+      userEmail: '',
+      userId: '',
+      token: ''
     }
+    componentDidMount() {
+      this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          // ..... DO YOUR LOGGED IN LOGIC
+          this.setState({ userEmail: user.email, userId: user.uid }, () => {
+            this.getFirebaseIdToken()
+          });
+        }
+        else {
+          // ..... The user is logged out
+        }
+      })
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+
+    handleUnprotectedAPI = (e) => {
+      const { userEmail, userId } = this.state;
+
+      axios.post('http://localhost:3001/unprotected', {
+        id: userId,
+        email: userEmail
+      })
+        .then(response => response.data)
+        .then(data => {
+          console.log(data);
+        })
+    }
+    getFirebaseIdToken = () => {
+      firebase.auth().currentUser.getIdToken(false).then((token) => {
+        this.setState({ token })
+      }).catch((error) => {
+        // Handle error
+      });
+    }
+
+    handleProtectedAPI = (e) => {
+      axios.post('http://localhost:3001/protected', { token: this.state.token })
+        .then(response => response.data)
+        .then(data => {
+          console.log(data);
+        })
+    }
+
   }
 
-  render(){
-    return(
-      <>
-      <div className='background'>
-      <h1>This is opening page</h1>
-      </div>
-      </>
-    )
+  render() {
+    const { userEmail, userId } = this.state;
+
+    if (userEmail === '') {
+      return <h1>You're not logged in</h1>
+    }
+    else {
+      return (
+        <>
+          <div className='row'>
+
+            <div className='col col-8'>
+              <img src={openingPic} alt="..." height="400" width="400" />
+            </div>
+
+            <div className='col col-4'>
+              <h1> login</h1>
+            </div>
+          </div>
+        </>
+      )
+    }
   }
 }
 
