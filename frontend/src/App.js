3fd14d9620aed0f8319d0fbc2b3ca743ead9a97b
Jennifer Perez <jenniferperez@pursuit.org>
2019-08-18 15:35:52 -0400

    added proper login and signup functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,58 @@
 import React, {Component} from 'react';
 import {Route, HashRouter } from "react-router-dom";
+import firebase from './firebase';
+
+
+//--Contexts
+import AuthContext from './contexts/auth';
+
+
+//--Pages
 import Header from './components/header';
 import Opening from '../src/components/opening';
 import Mainpage from '../src/components/mainpage2';
-
-
+import Logout from '../src/components/logout';
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      user:null,
+    }
+  }
+  componentDidMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      console.log(user, this.state)
+      
+      if (user) {
+        this.setState({ user: user.email }); //user shows user is logged in--->passed to auth context
+      } 
+      else {
+        this.setState({ user: null })
+      }
+    })
+  }
+
+  //invoking it to take it off memory
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+  
   render(){
     
   return (
-    
-    <HashRouter>
     <>
-    <Route path ="/opening" exact component={Opening}/>
-    {/* <Route path='/' component={ Header } /> */}
-    <div className='container mt-5'>
-    {/* <Route path ="/opening" exact component={Opening}/> */}
-    <Route path ="/" exact component={Mainpage}/>
-    </div>
-    </>
-    </HashRouter>
+        <HashRouter>
+        <AuthContext.Provider value={this.state.user}>
+          <Header/>
+        <Route path ="/opening" exact component={Opening}/>
+         <Route path ="/" exact component={Mainpage}/>
+         <Route path='/logout' exact component={ Logout } />
   
+        </AuthContext.Provider>
+        </HashRouter>
+      
+   </>
   
 
   )
